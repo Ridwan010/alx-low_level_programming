7ba5cb766cfd38aae2dea5aa10cc4cd737dfa5ba
Azeez Ridwan <azeezridwan57@gmail.com>
2022-06-30 14:14:55 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Prints out the last part of a quote in standard error
+ * main - Prints out the last part of a quote in standard error.
  *
- * Return: 1 if success
+ * Return: 1 if success.
  *
  */
 int main(void)
