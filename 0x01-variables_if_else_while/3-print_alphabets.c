42abf90c636c5342b79883e8b0a3498e6aa24b58
Azeez Ridwan <azeezridwan57@gmail.com>
2022-07-01 11:34:33 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,8 +9,9 @@
 int main(void)
 {
 	char letter;
-	for(letter = 'a'; letter <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
+	putchar('\n');
 	for (letter = 'A'; letter <= 'Z'; letter++)
 		putchar(letter);
 	putchar('\n');
