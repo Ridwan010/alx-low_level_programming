785c11569a8f09a76ce54b0b4672fc4e3e9352bd
Azeez Ridwan <azeezridwan57@gmail.com>
2022-07-01 14:12:13 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,7 +8,6 @@
 int main(void)
 {
 	char letter;
-	
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		if (letter != 'e' && letter != 'q')
@@ -16,5 +15,4 @@ int main(void)
 	}
 		putchar('\n');
 		return (0);
-}
-		
+}		
