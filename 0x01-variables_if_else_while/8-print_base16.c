6ce24b61eb13db7b94620b9654ab0e75491e3fdf
Azeez Ridwan <azeezridwan57@gmail.com>
2022-07-01 23:18:23 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,12 +7,11 @@
  */
 int main(void)
 {
-      int num;
-      char letter;
-  
-      for (num = 0; num < 10; num++)
-              putchar((num % 10) + '0');
-      putchar('\n')
-        
-      return (0);
+	int num;
+	char letter;
+	
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	putchar('\n');
+	return (0);
 }
